88933038448b45f6bc3f630335fdc97f3141fea1
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2026-09-03 10:02:47 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -1,17 +1,19 @@
-import mongoose from 'mongoose';
-import autoIncrement from 'mongoose-auto-increment';
+const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-auto-increment");
 
-// how our document look like
+// How our document looks
 const userSchema = mongoose.Schema({
-    name: String,
-    username: String,
-    email: String,
-    phone: Number
+  name: String,
+  username: String,
+  email: String,
+  phone: Number,
 });
 
 autoIncrement.initialize(mongoose.connection);
-userSchema.plugin(autoIncrement.plugin, 'user');
-// we need to turn it into a model
-const postUser = mongoose.model('user', userSchema);
 
-export default postUser;
+userSchema.plugin(autoIncrement.plugin, "user");
+
+// Create model
+const postUser = mongoose.model("user", userSchema);
+
+module.exports = postUser;
