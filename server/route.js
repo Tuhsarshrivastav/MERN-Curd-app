678352ff6a71a49a72980907f38a8da7de1a3367
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2026-09-03 10:02:13 +0530

    Update route.js

--- server/route.js
+++ server/route.js
@@ -1,12 +1,19 @@
-import express from 'express';
-import { getUsers, addUser, getUserById, editUser, deleteUser } from '../controller/user-controller.js';
+const express = require("express");
+
+const {
+  getUsers,
+  addUser,
+  getUserById,
+  editUser,
+  deleteUser,
+} = require("../controller/user-controller");
 
 const router = express.Router();
 
-router.get('/', getUsers);
-router.post('/add', addUser);
-router.get('/:id', getUserById);
-router.put('/:id', editUser);
-router.delete('/:id', deleteUser);
+router.get("/", getUsers);
+router.post("/add", addUser);
+router.get("/:id", getUserById);
+router.put("/:id", editUser);
+router.delete("/:id", deleteUser);
 
-export default router;
+module.exports = router;
