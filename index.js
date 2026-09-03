ba1450efe08e034cd6c024e5b9163e32f5e6fb66
Tushar Shrivastav <74534919+Tuhsarshrivastav@users.noreply.github.com>
2026-09-03 10:10:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,28 +10,25 @@ dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/users", Routes);
 
-const URL =
-  "mongodb://user:password@crud-shard-00-00.zgrax.mongodb.net:27017,crud-shard-00-01.zgrax.mongodb.net:27017,crud-shard-00-02.zgrax.mongodb.net:27017/CRUDAPP?ssl=true&replicaSet=atlas-qnhkkq-shard-0&authSource=admin&retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
+const URL = process.env.MONGO_URL;
 
 mongoose
-  .connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  .connect(URL)
   .then(() => {
+    console.log("MongoDB connected successfully");
+
     app.listen(PORT, () => {
       console.log(`Server is running on PORT: ${PORT}`);
     });
   })
   .catch((error) => {
-    console.log("Error:", error.message);
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
   });
